ed008dab0cd7aa0ab885bae78a8611a11d7c986e
tangpeng <tang.peng5@zte.com.cn>
2017-02-20 23:14:10 +0800

    game of life

---
+++ src/main/java/com/zte/ums/tennis/GameOfLife.java
@@ -0,0 +1,7 @@
+package com.zte.ums.tennis;
+
+/**
+ * Created by tangpeng on 2017/2/20.
+ */
+public class GameOfLife {
+}
---
+++ src/test/java/com/zte/ums/tennis/TestGameOfLife.java
@@ -0,0 +1,8 @@
+package com.zte.ums.tennis;
+
+/**
+ * Created by tangpeng on 2017/2/20.
+ */
+public class TestGameOfLife {
+    
+}
